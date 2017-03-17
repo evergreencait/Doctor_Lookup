e714270c1593a56fe9bde39bdb2bff44d7860cd1
Caitlin Hines <caitlinhines@me.com>
2017-03-17 13:53:47 -0700

    able to display doctor's first name

--- js/doctor.js
+++ js/doctor.js
@@ -4,10 +4,10 @@ var apiKey = require('./../.env').apiKey;
 Doctor = function(){
 }
 
-Doctor.prototype.getDoctor = function(medicalIssue, displayDoctor) {
+Doctor.prototype.getDoctor = function(medicalIssue, displayDoctorFirstName, displayDoctorLastName) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
-    displayDoctor(medicalIssue, response.data);
-    console.log(response.data);
+    displayDoctor(medicalIssue, response.data[0].profile.first_name);
+    console.log(response.data[0].profile.first_name);
   }).fail(function(error) {
     $('.findDoctor').text(error.responseJSON.message);
   });
