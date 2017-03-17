74e9ff276bc8a659fc27a2266a3d65eb830ab6ea
Caitlin Hines <caitlinhines@me.com>
2017-03-17 10:44:40 -0700

    able to return medical issue and console log response

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,12 +1,26 @@
-var Doctor = require('./../js/doctor.js').doctorModule;
+// var Doctor = require('./../js/doctor.js').doctorModule;
+//
+// $(document).ready(function() {
+//   var currentDoctorObject = new Doctor();
+//   $('#doctorLookup').click(function() {
+//     var medicalIssue = $('#issue').val();
+//     $('#issue').val("");
+//     var doctor = currentDoctorObject.getDoctor(medicalIssue);
+//     $('.findDoctor').text("The best doctor for " + medicalIssue + " is " + doctor);
+//     console.log(doctor);
+//   });
+// });
+
+
+var apiKey = "6a396573667d20353d487e33aa06a471";
 
 $(document).ready(function() {
-  var currentDoctorObject = new Doctor();
   $('#doctorLookup').click(function() {
     var medicalIssue = $('#issue').val();
     $('#issue').val("");
-    var doctor = currentDoctorObject.getDoctor(medicalIssue);
-    $('.findDoctor').text("The best doctor for " + medicalIssue + " is " + doctor);
-    console.log(doctor);
+    $('.findDoctor').text("The medical issue you have chosen is " + medicalIssue + ".");
+     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey, function(response) {
+       console.log(response);
+   });
   });
 });
