4e6d5e50280e0c91fc2ea889fe9b93d11bb33773
Caitlin Hines <caitlinhines@me.com>
2017-03-17 12:19:28 -0700

    still undefined:

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,26 +1,14 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
+var displayDoctor = function(medicalIssue, doctorData) {
+  $('.findDoctor').text("The doctor for " + medicalIssue + " is " + doctorData);
+};
+
 $(document).ready(function() {
   var currentDoctorObject = new Doctor();
   $('#doctorLookup').click(function() {
     var medicalIssue = $('#issue').val();
     $('#issue').val("");
-    var doctor = currentDoctorObject.getDoctor(medicalIssue);
-    $('.findDoctor').text("The best doctor for " + medicalIssue + " is " + doctor);
-    // console.log(doctor);
+    currentDoctorObject.getDoctor(medicalIssue, displayDoctor);
   });
 });
-
-
-// var apiKey = "6a396573667d20353d487e33aa06a471";
-//
-// $(document).ready(function() {
-//   $('#doctorLookup').click(function() {
-//     var medicalIssue = $('#issue').val();
-//     $('#issue').val("");
-//     $('.findDoctor').text("The medical issue you have chosen is " + medicalIssue + ".");
-//      $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey, function(response) {
-//        console.log(response);
-//    });
-//   });
-// });
--- js/doctor.js
+++ js/doctor.js
@@ -1,17 +1,18 @@
 var apiKey = require('./../.env').apiKey;
 
 function Doctor(){
+  this.doctorList = [];
 }
 
 
-Doctor.prototype.getDoctor = function(medicalIssue) {
+Doctor.prototype.getDoctor = function(medicalIssue, displayDoctor) {
+  var doctors = [];
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
-    $('.findDoctor').text("The best doctor for " + medicalIssue + " is " + response.data.profile.);
-    console.log("this works " + response)
+    displayDoctor(medicalIssue, response.data.profile.first_name);
+    console.log(response.data.profile);
   }).fail(function(error) {
     $('.findDoctor').text(error.responseJSON.message);
   });
 };
-
 
 exports.doctorModule = Doctor;
