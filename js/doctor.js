1055c467534450291f89dfad9211c006a65f7038
Caitlin Hines <caitlinhines@me.com>
2017-03-17 15:35:29 -0700

    styled button and added commented out for loop try

--- js/doctor.js
+++ js/doctor.js
@@ -14,3 +14,17 @@ Doctor.prototype.getDoctor = function(medicalIssue, displayDoctor) {
 }
 
 exports.doctorModule = Doctor;
+
+// Doctor.prototype.getDoctor = function(medicalIssue, displayDoctor) {
+//   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
+//     for(var i = 0; i < response.length; ii) {
+//
+//       displayDoctor(medicalIssue, response.data[0].profile.first_name, response.data[0].profile.last_name, response.data[0].profile.title);
+//       console.log(response.data[0].profile.first_name);
+//     }).fail(function(error) {
+//       $('.findDoctor').text(error.responseJSON.message);
+//     });
+//   }
+// }
+//
+// exports.doctorModule = Doctor;
