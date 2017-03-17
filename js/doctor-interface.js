d09eed3254b819f189964d0878e1cb9b8ffae960
Caitlin Hines <caitlinhines@me.com>
2017-03-17 08:33:50 -0700

    add get request to doctor-interface.js for doctor api key

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,16 +1,15 @@
 var apiKey = require('./../.env').apiKey;
 var Doctor = require('./../js/doctor.js').doctorModule;
 
-// $(document).ready(function() {
-//   var currentDoctorObject = new Doctor();
-//   currentDoctorObject.getDoctor();
-//   $('#doctorLookup').click(function() {
-//     var city = $('#issue').val();
-//     $('#issue').val("");
-//     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-//       $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-//     }).fail(function(error) {
-//       $('.showWeather').text(error.responseJSON.message);
-//     });
-//   });
-// });
+
+$(document).ready(function() {
+  $('#doctorLookup').click(function() {
+    var city = $('#issue').val();
+    $('#issue').val("");
+
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey, function(response) {
+    });
+
+    console.log("Notice: The GET request has been made.");
+  });
+});
