f9fddef3e3172e4392ad17973e4b70303fc114d3
Caitlin Hines <caitlinhines@me.com>
2017-03-17 09:40:37 -0700

    able to print medical issue but doctor is undefined

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,14 +1,12 @@
-var apiKey = require('./../.env').apiKey;
 var Doctor = require('./../js/doctor.js').doctorModule;
 
-
 $(document).ready(function() {
+  var currentDoctorObject = new Doctor();
   $('#doctorLookup').click(function() {
     var medicalIssue = $('#issue').val();
     $('#issue').val("");
-    $('.findDoctor').text("The issue you have chosen is " + medicalIssue + ".");
-    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey, function(response) {
-      console.log(response);
-    });
+    var doctor = currentDoctorObject.getDoctor(medicalIssue);
+    $('.findDoctor').text("The best doctor for " + medicalIssue + " is " + doctor);
+    console.log(doctor);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,9 +1,15 @@
+var apiKey = require('./../.env').apiKey;
+
 function Doctor(){
 }
 
 
-Doctor.prototype.getDoctor = function() {
-  console.log("Hi! I'm a doctor object!");
+Doctor.prototype.getDoctor = function(medicalIssue) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
+    $('.findDoctor').text("The best doctor for " + medicalIssue + " is " + response.profile.first_name + "%");
+  }).fail(function(error) {
+    $('.findDoctor').text(error.responseJSON.message);
+  });
 };
 
 
