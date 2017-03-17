52217c3962900577936fdaf378d43585fdad50b9
Caitlin Hines <caitlinhines@me.com>
2017-03-17 12:23:49 -0700

    returning whole object - need to make a for loop

--- js/doctor.js
+++ js/doctor.js
@@ -8,7 +8,7 @@ function Doctor(){
 Doctor.prototype.getDoctor = function(medicalIssue, displayDoctor) {
   var doctors = [];
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
-    displayDoctor(medicalIssue, response.data.profile.first_name);
+    displayDoctor(medicalIssue, response.data);
     console.log(response.data.profile);
   }).fail(function(error) {
     $('.findDoctor').text(error.responseJSON.message);
