7367b2148d14d084c257a937fe84b6b2213002e9
Caitlin Hines <caitlinhines@me.com>
2017-03-22 20:17:33 -0700

    add 'empty' to remove div before appending

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,13 +1,13 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayDoctor = function(medicalIssue, doctorFirst, doctorLast, doctorTitle) {
-
-  $('.findDoctor').append("<li>" + doctorFirst + " " + doctorLast + "</li>");
+  $('.findDoctor').append("<li>" + doctorFirst + " " + doctorLast);
 }
 
 $(document).ready(function() {
   var currentDoctorObject = new Doctor();
   $('#doctorLookup').click(function() {
+    $( ".findDoctor" ).empty();
     var medicalIssue = $('#issue').val();
     $('#issue').val("");
     currentDoctorObject.getDoctor(medicalIssue, displayDoctor);
--- js/doctor.js
+++ js/doctor.js
@@ -2,19 +2,19 @@
 var apiKey = require('./../.env').apiKey;
 
 Doctor = function(){
-}
+};
 
 Doctor.prototype.getDoctor = function(medicalIssue, displayDoctor) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
     for (var i = 0; i <= response.data.length; i++) {
 
       displayDoctor(medicalIssue, response.data[i].profile.first_name, response.data[i].profile.last_name, response.data[i].profile.title);
-      console.log(response.data[i]);
+      console.log(response.data[i].profile.title);
     }
   }).fail(function(error) {
     $('.findDoctor').text(error.responseJSON.message);
   });
-}
+};
 
 exports.doctorModule = Doctor;
 
